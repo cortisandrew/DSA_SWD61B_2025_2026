d803bba6b6bc756be5f6d199fcd8108d97ac9239
andrew.cortis@mcast.edu.mt <andrew.cortis@mcast.edu.mt>
2026-02-12 12:04:32 +0100

    Added comparison of ref and value types.

--- Ref_vs_Value_Types_Example/Program.cs
+++ Ref_vs_Value_Types_Example/Program.cs
@@ -1,3 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Ref_vs_Value_Types_Example;
+
 Console.WriteLine("Hello, World!!!");
-Console.ReadKey();
+
+// Declare and assign two integers
+// (value type variables)
+int a;
+int b;
+
+a = 5;
+b = 6;
+
+Console.WriteLine("Value type behaviour");
+Console.WriteLine("Declared a to 5 and b to 6");
+Console.WriteLine($"The value of {nameof(a)} is {a}");  // 5
+Console.WriteLine($"The value of {nameof(b)} is {b}");  // 6
+
+a = b;
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("a = b");
+Console.WriteLine($"The value of {nameof(a)} is {a}");  // 6
+Console.WriteLine($"The value of {nameof(b)} is {b}");  // 6
+
+b = 10;
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("b = 10");
+Console.WriteLine($"The value of {nameof(a)} is {a}");  // 6
+Console.WriteLine($"The value of {nameof(b)} is {b}");  // 10
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Reference type behaviour");
+Console.WriteLine("Declared a_Ref to 5 and b_Ref to 6");
+
+RefTypeClass a_Ref = new RefTypeClass() { Value = 5 };
+RefTypeClass b_Ref = new RefTypeClass() { Value = 6 };
+
+Console.WriteLine($"The value of {nameof(a_Ref)} " +
+    $"is {a_Ref.Value}");  // 5
+Console.WriteLine($"The value of {nameof(b_Ref)} " +
+    $"is {b_Ref.Value}");  // 6
+
+a_Ref = b_Ref;
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"The value of {nameof(a_Ref)} " +
+    $"is {a_Ref.Value}");  // 6
+Console.WriteLine($"The value of {nameof(b_Ref)} " +
+    $"is {b_Ref.Value}");  // 6
+
+b_Ref.Value = 10;
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"The value of {nameof(a_Ref)} " +
+    $"is {a_Ref.Value}");  // 10
+Console.WriteLine($"The value of {nameof(b_Ref)} " +
+    $"is {b_Ref.Value}");  // 10
+
+a_Ref = new RefTypeClass() { Value = 2 };
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"The value of {nameof(a_Ref)} " +
+    $"is {a_Ref.Value}");  // 2
+Console.WriteLine($"The value of {nameof(b_Ref)} " +
+    $"is {b_Ref.Value}");  // 10
+
---
+++ Ref_vs_Value_Types_Example/RefTypeClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_vs_Value_Types_Example
+{
+    public class RefTypeClass
+    {
+        public int Value { get; set; }
+    }
+}
