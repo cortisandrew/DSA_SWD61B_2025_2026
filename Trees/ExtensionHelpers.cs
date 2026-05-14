400452050d120b15eccf644c20abbd504e27180d
andrew.cortis@mcast.edu.mt <andrew.cortis@mcast.edu.mt>
2026-05-14 11:21:14 +0200

    Added HeapSort

--- Trees/ExtensionHelpers.cs
+++ Trees/ExtensionHelpers.cs
@@ -24,5 +24,29 @@ namespace Trees
             myList[i] = myList[j];
             myList[j] = temp;
         }
+
+        public static List<int> HeapSort(this List<int> myList)
+        {
+            // build an empty array to store the sorted elements (same size as the input list)
+            List<int> sorted = new List<int>(myList.Count);
+
+            Heap heap = new Heap();
+
+            // Build a heap by adding all values, one by one
+            foreach (var intValue in myList)    // xn elements
+            {
+                heap.Add(intValue);             // O(log n) time
+            }
+            // foreach total time: O(n log n)
+
+            while (heap.Count > 0)              // xn elements
+            {
+                int minValue = heap.RemoveMin(); // O(log n) time
+                sorted.Add(minValue);             // O(1) time
+            }
+            // while total time: O(n log n)
+
+            return sorted; // total time: O(n log n)
+        }
     }
 }
